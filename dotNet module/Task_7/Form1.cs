73d40769b1546cb8fbd4c333ebd17ec9931d50d4
List0chek <vladislavsky88@gmail.com>
2021-03-15 00:22:55 +0400

    Сделал задание 7.

---
+++ dotNet module/Task_7/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string defaultPath = "q2.rtf.gz";
+
+        private void GetTextButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opfd = new OpenFileDialog();
+            if (File.Exists(defaultPath))
+            {
+                GZIPTextReader.LoadGZippedText(defaultPath, richTextBox1);
+            }
+            else if (opfd.ShowDialog() == DialogResult.OK)
+            {
+                GZIPTextReader.LoadGZippedText(opfd.FileName, richTextBox1);
+            }
+        }
+    }
+}
---
+++ dotNet module/Task_7/GZIPTextReader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace Task_7
+{
+    public class GZIPTextReader
+    {
+        /// <summary>
+        /// Метод позволяет загрузить данные из текстового файла, сжатого по методу GZIP.
+        /// </summary>
+        public static void LoadGZippedText(string filename, RichTextBox edit)
+        {
+            using (var sourceStream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+
+            using (var uncompressedStream = new GZipStream(sourceStream, CompressionMode.Decompress, true))
+
+            using (var textReader = new StreamReader(uncompressedStream, true))
+
+            edit.Rtf = textReader.ReadToEnd();
+        }
+    }
+}
