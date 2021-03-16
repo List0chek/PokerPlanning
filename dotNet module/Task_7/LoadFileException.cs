83e79716bdec4f8824698464500db21a2b133225
List0chek <vladislavsky88@gmail.com>
2021-03-16 11:39:00 +0400

    Сделал задание 8

---
+++ dotNet module/Task_7/LoadFileException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_4;
+
+namespace Task_7
+{
+    [Serializable]
+    public class LoadFileException : Exception
+    {
+        public LoadFileException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ dotNet module/Task_8/TextFileBruteForceEnumerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task_8
+{
+    public class TextFileBruteForceEnumerator : IEnumerator<string>
+    {
+        private int curIndex = -1;
+
+        public List<string> collection = new List<string>();
+
+        
+        public TextFileBruteForceEnumerator(List<string> collection)
+        {
+            this.collection = collection;
+        }
+
+        /// <summary>
+        /// Возвращает текущий эелемент.
+        /// </summary>
+        public string Current
+        {
+            get
+            {
+                try
+                {
+                    return this.collection[this.curIndex];
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        /// <summary>
+        /// Перемещает указатель на след. элемент, делает проверку на окончание листа.
+        /// </summary>
+        public bool MoveNext()
+        {
+            this.curIndex++;
+            if (this.curIndex >= collection.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Сброс указателя.
+        /// </summary>
+        public void Reset() { curIndex = -1; }
+
+        void IDisposable.Dispose() { }
+    }
+}
+
