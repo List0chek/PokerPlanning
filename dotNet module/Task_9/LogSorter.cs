f67bd8aa9694da49675421d42c61e576470c506e
List0chek <vladislavsky88@gmail.com>
2021-03-17 19:07:59 +0400

    Сделал задание 9.

---
+++ dotNet module/Task_9/LogSorter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Task_8;
+
+namespace Task_9
+{
+    public class LogSorter
+    {
+        /// <summary>
+        /// Метод сортирует записи из лога за определенную дату по времени.
+        /// </summary> 
+        public static List<string> GetRecordsSorted(string path, DateTime dateStart)
+        {
+            var textFileBruteForce = new TextFileBruteForce(path);
+            int dateLength = "dd.MM.yyyy\tHH:mm:ss".Length;
+            DateTime dateTime;
+
+            var scoreQuery = textFileBruteForce.Where(line => DateTime.TryParseExact(line.Substring(0, dateLength), "dd.MM.yyyy\tHH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime))
+                                               .Where(line => DateTime.Parse(line.Substring(0, dateLength)).Date == dateStart.Date)
+                                               .OrderBy(line => DateTime.Parse(line.Substring(0, dateLength)));
+            return scoreQuery.ToList();
+        }
+    }
+}
---
+++ dotNet module/Task_9/TextFileBruteForce.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task_8
+{
+    public class TextFileBruteForce : IEnumerable<string>, IDisposable
+    {
+        private string path;
+
+        public TextFileBruteForce(string path)
+        {
+            this.path = path;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new TextFileBruteForceEnumerator(this.path);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        // Этот код добавлен для правильной реализации шаблона высвобождаемого класса.
+        public void Dispose()
+        {
+        }
+    }
+}
