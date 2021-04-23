0cf60fda9593f1225a4e9dcad51f0aa82f5fcee3
List0chek <vladislavsky88@gmail.com>
2021-04-23 09:08:08 +0400

    app.tsx fix, gitignore upd., package upd.)

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import FirstPage from "../Pages/FirstPage/firstPage";
 import InvitePage from "../Pages/InvitePage/invitePage";
-import PlanningPage from "../Pages/PlanningPage/planningPage";
-import ResultPage from "../Pages/ResultPage/resultPage";
-import ResultPageEnterStory from "../Pages/ResultPage-EnterStory/resultPageEnterStory";
-import Modal from "../Pages/Modal/modal";
 import RoomPage from "../Pages/RoomPage/roomPage";
 import "./app.css";
 
@@ -12,24 +8,5 @@ import "./app.css";
 function App() {
   return (<RoomPage />);
 }
-
-/*const storyDetailsData = [
-  {
-    username: "testtesttesttesttesttest 1",
-    value: "3"
-  },
-  {
-    username: "test 2",
-    value: "3"
-  },
-  {
-    username: "test 3",
-    value: "5"
-  }
-];
-
-function App() {
-  return (<Modal playersList={storyDetailsData}/>);
-}*/
 
 export default App;
